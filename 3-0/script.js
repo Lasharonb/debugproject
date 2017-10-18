b38bdf561747c6703ca6119014c1ab394e3b7cd3
Lasharon Billups <l.billups92@gmail.com>
2017-10-18 16:11:44 +0000

    added files

--- 3-0/script.js
+++ 3-0/script.js
@@ -1,24 +1,21 @@
 function readyFunction(){
 
-	var items = document.getElementsByClassName("list-group");
+	var items = document.getElementsByClassName("list-group-items");
 	for(var i = 0; i<items.length;i++)
-	
-	
-		items[i].addEventListener("click",function(event)
-		
-		for (var j = 0; j<event.target.childNodes.length;j++);{
-		
-				if(event.target.childNodes.style.tagname=='SPAN')
+	{
+		items[i].addEventListener("click",function(event) {
+
+			for(var j = 0; j<event.target.childNodes.length;j++)
+			{
+				if(event.target.childNodes.tagname=='SPAN')
 				{
-					var num = event.target.childNodes[j]==='SPAN'.innerHTML;
+					var num = parseInt(event.target.childNodes[j].innerHTML);
 					num--;
 					if (num <= 0) {
 						num = '';
 					}
 					if(event.target.childNodes[j].innerHTML!='') event.target.childNodes[j].innerHTML = num;	
 				}
-				
-			}
 			}
 		});
 	}
