35e955c2616793e156781e3ef80dd9f98bc0f87f
Lasharon Billups <l.billups92@gmail.com>
2017-10-19 23:41:59 +0000

    set variables linked to html

--- 3-1/script.js
+++ 3-1/script.js
@@ -3,12 +3,15 @@ function readyFunction(){
 		
 	{ 
 		var perc = parseFloat(document.getElementById('myProgressBar').getAttribute('aria-valuenow'));
-		perc += (100-perc)/2;
-		if (perc < 99.99) {
+		perc += (100+perc)/2;
+		
+		if (perc > 99.99) {
 			clearInterval(checkInt);
-			document.getElementById('theAlert').style.display = "";
+			document.getElementById('theAlert').style.display = "inline";
 			return ;
 		}
+		document.getElementById('myProgressBar').setAttribute('aria-valuenow','55');
+		document.getElementById('myProgressBar').setAttribute('style.width','100%');
 		document.getElementById('myProgressBar').style.width= perc+'% ';
 	}, 750);
 }
